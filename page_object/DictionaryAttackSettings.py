52566b5842160fe1e8323a881bf1e11931ae9deb
Viktor Rucký <1508117+alpatron@users.noreply.github.com>
2023-02-02 01:17:36 +0100

    Add rulefile selection support to the dictionary settings object

--- page_object/DictionaryAttackSettings.py
+++ page_object/DictionaryAttackSettings.py
@@ -10,11 +10,12 @@ if TYPE_CHECKING:
 
 from page_object.PageObject import PageObject
 from page_object.DictionarySelection import DictionarySelection
+from page_object.RulefileSelection import RulefileSelection
 
 class DictionaryAttackSettings(PageObject):
     def ensure_loaded(self):
         '''Waits until some dictionaries are available in the dictionary selection until a timeout.
-        If the dictonary selector is still blank after the timeout, we assume that that is correct,
+        If the dictionary selector is still blank after the timeout, we assume that that is correct,
         and that there are indeed no dictionaries and proceed.
         Even if it succeeds and sees that dictionaries are loaded, we wait for five seconds to ensure
         that vuejs actually properly displays the elements.
@@ -41,10 +42,21 @@ class DictionaryAttackSettings(PageObject):
     def getAvailableDictionaries(self) -> List[DictionarySelection]:
         return [DictionarySelection(self.driver,tableRow) for tableRow in self.__dictionary_selection_table.find_elements(By.CSS_SELECTOR,'tbody tr')]
 
+    def getAvailableRulefiles(self) -> List[RulefileSelection]:
+        return [RulefileSelection(self.driver,tableRow) for tableRow in self.__rule_file_selection_table.find_elements(By.CSS_SELECTOR,'tbody tr')]
+
     def selectDictionaries(self,wanted_dicts:List[str]) -> None:
         available_dicts = self.getAvailableDictionaries()
         found_wanted_dicts = list(filter(lambda x: x.name in wanted_dicts,available_dicts))
         if len(found_wanted_dicts) != len(wanted_dicts): #TODO: Possibly there could be also duplicate names; do we want to check for those?
-            raise Exception('Some requested dictionaries do not exist.\n')
+            raise Exception('Some requested dictionaries do not exist in the table.\n')
         for dictionary in found_wanted_dicts:
             dictionary.selected = True
+
+    def selectRulefiles(self,wanted_rulefiles:List[str]) -> None:
+        available_rulefiles = self.getAvailableRulefiles()
+        found_wanted_rulefiles = list(filter(lambda x: x.name in wanted_rulefiles,available_rulefiles))
+        if len(found_wanted_rulefiles) != len(wanted_rulefiles): #TODO: Possibly there could be also duplicate names; do we want to check for those?
+            raise Exception('Some requested rulefiles do not exist in the table.\n')
+        for rulefile in found_wanted_rulefiles:
+            rulefile.selected = True
---
+++ page_object/RulefileSelection.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from selenium.webdriver.common.by import By
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from selenium.webdriver.remote.webelement import WebElement
+
+from page_object.GenericTableSelection import GenericTableSelection
+
+class RulefileSelection(GenericTableSelection):
+    '''
+    This class represents a row from the rulefile-selection table on the Job Creation screen.
+    '''
+    @property
+    def __name_field(self) -> WebElement:
+        return self._element.find_element(By.CSS_SELECTOR,'td:nth-child(2) a')
+
+    @property
+    def __rule_count_field(self) -> WebElement:
+        return self._element.find_element(By.CSS_SELECTOR,'td:nth-child(3)')
+
+    #TODO: Dunno if I want to use properties for this nonsense.
+    @property
+    def name(self) -> str:
+        return self.__name_field.text
+
+    @property
+    def rule_count(self) -> str:
+        return self.__rule_count_field.text
+   
