fdcee7bcc6599555ad01cfde3ede4184b02dbfed
Viktor Rucký <1508117+alpatron@users.noreply.github.com>
2023-02-23 16:12:31 +0100

    Typos and formatting

--- test/page_object/common/page_object.py
+++ test/page_object/common/page_object.py
@@ -7,7 +7,7 @@ if TYPE_CHECKING:
     from selenium.webdriver.remote.webdriver import WebDriver
 
 class PageObject:
-    '''This is the base class of all page objects.
+    """This is the base class of all page objects.
     Page objects represent individual pieces of the UI and expose methods that model user
     interactions, like "login", "set job name", "create job" etc.
 
@@ -17,10 +17,11 @@ class PageObject:
     To create a page object, one needs to pass a Selenium WebDriver instance.
     When creating a page object, the `ensure_loaded` method is automatically run to wait until
     the UI is loaded (this method needs to be implemented for each page object;
-    see the `ensure_loaded` doc string). You can supress the running of `ensure_loaded` by
+    see the `ensure_loaded` doc string). You can suppress the running of `ensure_loaded` by
     specifying using the parameter `no_ensure_loaded=True` when creating a page object.
-    This is neccessary when creating the first object in a script, as the web browser starts on
-    blank page.'''
+    This is necessary when creating the first object in a script, as the web browser starts on
+    blank page.
+    """
 
     def __init__(self,driver:WebDriver,no_ensure_loaded=False):
         self.driver = driver
@@ -28,16 +29,18 @@ class PageObject:
             self.ensure_loaded()
 
     def ensure_loaded(self) -> None:
-        '''Waits until the UI the page object models is loaded and it is safe to use the
+        """Waits until the UI the page object models is loaded and it is safe to use the
         page object. This is an abstract method that can but need not be implemented.
-        The default, non-overriden method performs no checks and returns immediately.
+        The default, non-overridden method performs no checks and returns immediately.
         
         This methods should return if and only if the state of the web app is ready,
-        and it should raise an exception if the UI isn't ready within a time limit.'''
+        and it should raise an exception if the UI isn't ready within a time limit.
+        """
 
     def _click_away(self) -> None:
-        '''Sometimes when testing, one needs to "click away" from, say, an input field to return
+        """Sometimes when testing, one needs to "click away" from, say, an input field to return
         to a neutral application state. For example, when typing into an input field, a pop-up
         may appear (for example when using the mask editor). You can use this method to
-        implement a click-away behaviour for use with your page objects.'''
+        implement a click-away behaviour for use with your page objects.
+        """
         raise NotImplementedError
